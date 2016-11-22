e97fb4f09688465919e65d82e9821704c8d460ec
Erik Hansen <erikpallhansen@gmail.com>
2016-11-22 15:34:03 -0600

    Only do explicit error string checking on M2 <= 2.1.0 when running setup:static-content:deploy

--- lib/capistrano/magento2.rb
+++ lib/capistrano/magento2.rb
@@ -44,16 +44,18 @@ module Capistrano
 
         # Clear the pipefail option from list of prefixes so it won't affect other commands
         SSHKit.config.command_map.prefix[:magento].pop
-
-        if not output.to_s.include? 'New version of deployed files'
-          raise Exception, "\e[0;31mFailed to compile static assets\e[0m"
-        end
-
-        output.to_s.each_line { |line|
-          if line.split('errors: ', 2).pop.to_i > 0
+        
+        if magento_version <= Gem::Version.new('2.1.0')
+          if not output.to_s.include? 'New version of deployed files'
             raise Exception, "\e[0;31mFailed to compile static assets\e[0m"
           end
-        }
+
+          output.to_s.each_line { |line|
+            if line.split('errors: ', 2).pop.to_i > 0
+              raise Exception, "\e[0;31mFailed to compile static assets\e[0m"
+            end
+          }
+        end
       end
     end
   end

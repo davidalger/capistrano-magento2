418aa0f689a0205fa69c965f69ddbaaff7b06a37
Josh Johnson <josh@grafikchaos.com>
2017-03-14 08:01:53 -0500

    Version bump to 0.6.1
    Fixes #64 issue with Magento version comparison and an apparent color
    bug.

--- lib/capistrano/magento2/version.rb
+++ lib/capistrano/magento2/version.rb
@@ -1,14 +1,14 @@
 ##
  # Copyright © 2016 by David Alger. All rights reserved
- # 
+ #
  # Licensed under the Open Software License 3.0 (OSL-3.0)
  # See included LICENSE file for full text of OSL-3.0
- # 
+ #
  # http://davidalger.com/contact/
  ##
 
 module Capistrano
   module Magento2
-    VERSION = '0.6.0'
+    VERSION = '0.6.1'
   end
 end
